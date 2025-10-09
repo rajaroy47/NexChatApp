44ce0e2fc3a68b6d5852371a58505e30656bd3bd
rajaroy47 <rajaroy6297818984@gmail.com>
2025-10-09 14:26:08 +0530

    Initial commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,14 @@
 // import { getDatabase } from 'firebase/database';
 // import { getAuth } from 'firebase/auth';
 
-// export const firebaseConfig = {
-//     apiKey: "AIzaSyBblpZdFi8E3Lcf-4yEoJ3yhUonWmgpgyc",
-//     authDomain: "nexchat-8c980.firebaseapp.com",
-//     projectId: "nexchat-8c980",
-//     storageBucket: "nexchat-8c980.appspot.com",
-//     messagingSenderId: "87765028514",
-//     appId: "1:87765028514:web:26a575692fa3e343e3491fb",
-//     databaseURL: "https://nexchat-8c980-default-rtdb.firebaseio.com",
+// const firebaseConfig = {
+//     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: import.meta.env.VITE_FIREBASE_APP_ID,
+//     databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
 // };
 
 // export const ADMIN_EMAIL = 'eduroket001@gmail.com';
