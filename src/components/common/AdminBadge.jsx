2b27c8a5073210fd42e1db37291b018f5b40b34f
Raja Roy <163350224+rajaroy47@users.noreply.github.com>
2026-03-06 11:41:01 +0530

    Delete src directory

--- src/components/common/AdminBadge.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const AdminBadge = () => (
-    <span className="w-fit ml-1 px-2 py-0.5 text-xs font-bold text-white bg-red-600 rounded-full shadow-md animate-pulse">
-      ADMIN
-    </span>
-);
-
-export default AdminBadge;
